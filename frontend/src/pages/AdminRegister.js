6e380138d3e96236d3bc3189477064e0050eeea8
Etaju3 <crs.vsl@outlook.com>
2024-06-04 19:59:02 +0100

    Update AdminRegister.js

--- frontend/src/pages/AdminRegister.js
+++ frontend/src/pages/AdminRegister.js
@@ -1,1 +1,9 @@
+import React from 'react';
+import Register from '../components/Register';
+
+const AdminRegister = () => {
+  return <Register role="admin" />;
+};
+
+export default AdminRegister;
 
