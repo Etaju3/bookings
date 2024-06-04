f7693b52083c16f71b4c2716bbc9615cdf1ed267
Etaju3 <crs.vsl@outlook.com>
2024-06-04 19:59:26 +0100

    Update ClientRegister.js

--- frontend/src/pages/ClientRegister.js
+++ frontend/src/pages/ClientRegister.js
@@ -1,1 +1,9 @@
+import React from 'react';
+import Register from '../components/Register';
+
+const ClientRegister = () => {
+  return <Register role="client" />;
+};
+
+export default ClientRegister;
 
