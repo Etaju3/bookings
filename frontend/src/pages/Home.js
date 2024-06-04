ae92a899f5d550e79bb405d27eaad3247d4b118c
Etaju3 <crs.vsl@outlook.com>
2024-06-04 19:58:36 +0100

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,1 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import BookingForm from '../components/BookingForm';
+
+const Home = () => {
+  return (
+    <div className="container">
+      <h1>Welcome to the Service Booking App</h1>
+      <BookingForm />
+      <div className="mt-3">
+        <Link to="/client/login">Client Login</Link> | <Link to="/admin/login">Admin Login</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
 
