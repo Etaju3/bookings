aaac123ad37e2a4e2e9782cd093de684a504769d
Etaju3 <crs.vsl@outlook.com>
2024-06-04 19:59:37 +0100

    Update NotFound.js

--- frontend/src/pages/NotFound.js
+++ frontend/src/pages/NotFound.js
@@ -1,1 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div className="container">
+      <h1>404 - Not Found</h1>
+    </div>
+  );
+};
+
+export default NotFound;
 
